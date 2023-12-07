82eb80e8a2f6675ab06e3e3523fe80aa943557f1
nct2309 <nctstuti@gmail.com>
2023-12-07 22:55:00 +0700

    Cook

--- components/search_result.php
+++ components/search_result.php
@@ -24,18 +24,14 @@
 </style>
 
 <div class="modal fade" id="exampleModalToggle" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="exampleModalToggleLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered modal-xl">
+  <div class="modal-dialog modal-xl modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
         <h1 class="modal-title fs-5" id="exampleModalToggleLabel" style="color: #026fd4;">RESULT</h1>
-        <div class="spinner-border text-primary m-1 p-2" style="width: 1.5rem; height: 1.5rem; display: none" role="status" id="loading" aria-hidden="true">
-                <span class="visually-hidden">Loading...</span>
-        </div>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
-        <div class="container-fluid px-3 table-responsive" id="table_content">
-        </div>
+      <div class="modal-body" id="table_content">
+
       </div>
       <div class="modal-footer">
       </div>
@@ -43,26 +39,18 @@
   </div>
 </div>
 <div class="modal fade" id="exampleModalToggle2" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="exampleModalToggleLabel2" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered modal-xl">
+  <div class="modal-dialog modal-xl modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
         <h1 class="modal-title fs-5" id="exampleModalToggleLabel2" style="color: #026fd4;">PATIENT INFORMATION</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
-        <div class="container-fluid" id="table_content_2">
-          <!-- content of view patient information -->
-          <?php
-            include_once ("view_info_test.php");
-            include_once ("view_info_full.php");
-          ?>
+      <div class="modal-body" id="table_content_2">
 
-        </div>
       </div>
       <div class="modal-footer">
         <button class="btn " data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Back to search result</button>
       </div>
     </div>
   </div>
-
-
+</div>
---
+++ register.php
@@ -0,0 +1,25 @@
+
+<?php
+    include_once($_SERVER["DOCUMENT_ROOT"] . "/QuarantineCampDB/config.php");
+
+    // Create a connection to the MySQL server
+    $conn = pg_connect("host=$host port=$port user=$username password=$password dbname=$dbname");
+
+    // Check if the connection is successful
+    if (!$conn) {
+        die("Connection failed: " . pg_last_error());
+    }
+
+    $manager_code = 2;
+    $username = "stuti@gmail.com";
+    $password = password_hash("123456", PASSWORD_DEFAULT);
+
+    $query = "INSERT INTO public.mgr_authentication VALUES ($1, $2, $3)";
+    $result = pg_query_params($conn, $query, array($manager_code, $username, $password));
+
+    if ($result !== false) {
+        echo "Insert sucess";
+    } else echo "Error: " . pg_last_error();
+    
+    pg_close($conn);
+?>
